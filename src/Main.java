a072c27254fe36928bdbb2554ac9b48e926fd686
Magdalena Sitek <msitek@objectivity.co.uk>
2020-08-17 22:17:04 +0200

    revert C3

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World master!");
         System.out.println("branch5 commit1");
+<<<<<<< HEAD
         System.out.println("branch5 commit2");
         System.out.println("branch5 commit3");
 
+=======
+>>>>>>> parent of d7440b5... branch5 commit2
     }
 }
