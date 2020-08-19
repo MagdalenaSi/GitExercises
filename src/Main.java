30773673710b970d270c74a171f57d16947fdc79
Magdalena Sitek <msitek@objectivity.co.uk>
2020-08-19 20:14:43 +0200

    branch2 commit3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World master!");
-        System.out.println("Branch 1 commit 1");
         System.out.println("Branch 2 commit 1");
+        System.out.println("Branch 2 commit 2");
+        System.out.println("Branch 2 commit 3");
     }
 }
