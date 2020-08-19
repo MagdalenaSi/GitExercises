7335c228b3a4c484cad48f28e82eb96b25e23ab3
Magdalena Sitek <msitek@objectivity.co.uk>
2020-08-19 20:14:43 +0200

    cherry-pick branch2 commit3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World master!");
+<<<<<<< HEAD
         System.out.println("branch3 commit1");
+=======
+        System.out.println("Branch 2 commit 1");
+        System.out.println("Branch 2 commit 2");
+        System.out.println("Branch 2 commit 3");
+>>>>>>> 3077367... branch2 commit3
     }
 }
