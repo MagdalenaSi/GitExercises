ca9b664aac16e06bc7e7448ea8f7ec7c1f467965
Magdalena Sitek <msitek@objectivity.co.uk>
2020-08-20 21:03:32 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World master!");
+        System.out.println("Hello World master!test");
         System.out.println("Branch 1 commit 1");
     }
 }
