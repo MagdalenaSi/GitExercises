ea07d264bc5c57b26613dffc70113b89eebaeb88
Magdalena Sitek <msitek@objectivity.co.uk>
2020-08-20 21:43:10 +0200

    next message branch 1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World master!test");
         System.out.println("Branch 1 commit 1");
-        System.out.println("Part to commit after merge conflict");
+        System.out.println("next message branch 1");
     }
 }
